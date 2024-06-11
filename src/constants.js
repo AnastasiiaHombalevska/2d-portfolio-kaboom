effc7e8ab774b8d255f8ea9abb3bad3d79586fd4
Anastasiia <vasilisandreeva00@gmail.com>
2024-06-11 12:55:26 +0300

    fixed the function of displaying dialogs, added the text of dialogs

--- src/constants.js
+++ src/constants.js
@@ -1,30 +1,32 @@
 export const scaleFactor = 4;
 
 export const dialogueData = {
-  pc: `This is my PC. I work mostly in React/TypeScript these days.
-    I've made a couple of tasks during my education proces. I also like to improve my JavaScript knolage by stading about asynhronic events. Anyway regardless of the language, I just like programming.
-    Here is my <a href="https://github.com/AnastasiiaHombalevska" target="_blank">Github</a>!`,
-  
-  "cs-degree": `This is my CS degree. I hung it on the wall because I'm proud of it. It was a very theoretical degree but I think it gave me a good foundation.`,
-  
-  "sofa-table": `That's my sofa. I like to relax here and watch YouTube. 
-    I also make game programming tutorials on YouTube. Go sub to <a href="https://youtube.com/@jslegenddev" target="_blank">my channel</a>! (If you like the content)
-    You'll learn how I built this portfolio you're currently playing through!`,
+  pc: `This is my computer. Now I'm mostly learning React with TypeScript.
+  I've done a few assignments throughout my studies, including topics on creating ToDo apps, paginated web pages, and more.
+  My goal is to move towards becoming a Fullstack developer.
+  Here is my <a href="https://github.com/AnastasiiaHombalevska" target="_blank">Github</a>!`,
 
-  tv: `That's my TV. I've been watching tech youtubers a lot recently like :
-  <a href="https://www.youtube.com/@ThePrimeTimeagen" target="_blank">Theprimeagen</a>, <a href="https://www.youtube.com/@t3dotgg" target="_blank">Theo - t3.gg</a>,
-  <a href="https://www.youtube.com/@PirateSoftware" target="_blank">PirateSoftware</a> (sometimes) and <a href="https://www.youtube.com/@MelkeyDev" target="_blank">Melkey</a>!`,
+  "cs-degree": `This is my diploma in psychology. Because working as a psychologist in the education system is a period of life in which there is something to remember and tell.`,
 
-  bed: `The best way to recover is to lie on the bed (on the floor is another great option &#128512).
+  "sofa-table": `That's my sofa. This is a very functional object. It is a work area, for example, the text you are reading now was written here. A place to relax with reading The Hobbit. A place to procrastinate by counting dots on the ceiling, and so on, depending on the request&#128512`,
+
+  tv: `That's my TV. I've been watching tech youtubers, art channels and training channels from very different areas:
+  <a href="https://www.youtube.com/@JSLegendDev" target="_blank">JSLegendDev</a>,
+  <a href="https://www.themysteztvo.com.ua/" target="_blank">The Mysteztvo.</a> It's not YouTube, but let's count it too,
+  <a href="https://www.youtube.com/@Guardinaction" target="_blank">(It's interesting, but it may not be clear)</a> and i think that's enough.`,
+
+  bed: `The best way to recover is to lie on the bed (on the floor is another great option&#128512).
   This is also where I sleep, of course. Great ideas come to me in the middle of the night. When an idea comes to me, I often have to write it down, otherwise I won't be able to sleep.`,
 
-  resume: `This is my desk and on it is my resume. <a href="https://github.com/JSLegendDev/Resume/blob/main/JSLegend%20Resume-1.pdf" target="_blank">Check it out?</a>
-  Contact me at jslegend@protonmail.com if you have any interesting job opportunities!`,
+  resume: `This is my desk and on it is my resume. <a href="https://docs.google.com/document/d/1aRCgISG6vP2Rt-9pi7w3LoNnpqjo1g0mJg7ivTFqDzY/edit?usp=sharing" target="_blank">Check it out?</a>
+  I guess I need to add this game to it as well.
+  <a href="mailto:vasilisandreeva00@gmail.com">Contact me</a> if you have any interesting job opportunities!`,
 
-  projects: `Info about this portfolio : It's made with the Kaboom.js library which is a library for making games in JavaScript.
-  Text is rendered with HTML/CSS. So the textbox you're currently reading is not rendered within canvas. <a href="https://youtube.com/@jslegenddev" target="_blank">here</a>.`,
+  projects: `Information about this portfolio : Created with Kaboom.js library, which is a library for creating games in JavaScript.
+  The text is rendered using HTML/CSS. Therefore, the text box you are reading now is not displayed on the canvas.
+  In general, this is an additional way to tell you that I am looking for my first job as a front-end developer`,
 
   library: `There are a lot of books on my shelves, including programming books. But i have a question. Who else compulsively buys books without ever finishing them?`,
 
-  exit: `If you want to exit this portfolio, just close the tab.`,
+  exit: `A kindly reminder: to contribute to Ukraine's victory - <a href="https://savelife.in.ua/donate/#donate-army-card-monthly" target="_blank">click here.</a> And if you want to exit this portfolio, just close the tab.`,
 };
